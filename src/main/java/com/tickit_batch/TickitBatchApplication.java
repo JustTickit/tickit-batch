3a395982cd0911f93add54a51f692597bce9bc3e
희은 <datakubit2@gmail.com>
2025-04-25 21:39:42 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/tickit_batch/TickitBatchApplication.java
@@ -0,0 +1,13 @@
+package com.tickit_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickitBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickitBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tickit_batch/TickitBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tickit_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickitBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
