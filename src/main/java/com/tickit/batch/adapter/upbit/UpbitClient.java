f263077ea2f23014d273157682f5a14fc1887e6a
희은 <datakubit2@gmail.com>
2025-06-12 19:24:54 +0900

    feat: Upbit API 연동용 UpbitClient 기본 구조 구현

--- src/main/java/com/tickit/batch/adapter/upbit/UpbitClient.java
+++ src/main/java/com/tickit/batch/adapter/upbit/UpbitClient.java
@@ -1,4 +1,13 @@
 package com.tickit.batch.adapter.upbit;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class UpbitClient {
+
+	private final WebClient webClient = WebClient.create("https://api.upbit.com");
 }
