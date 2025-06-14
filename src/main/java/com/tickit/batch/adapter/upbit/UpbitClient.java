1109e42157bf1a35af0301690daf4c1e05ba6a72
희은 <datakubit2@gmail.com>
2025-06-14 20:05:37 +0900

    feat: Upbit 시세 API 연동 및 응답 DTO 구현

--- src/main/java/com/tickit/batch/adapter/upbit/UpbitClient.java
+++ src/main/java/com/tickit/batch/adapter/upbit/UpbitClient.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.tickit.batch.adapter.upbit.dto.MarketCodeResponse;
+import com.tickit.batch.adapter.upbit.dto.TickerResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,6 +21,19 @@ public class UpbitClient {
 			.uri("/v1/market/all?isDetails=false")
 			.retrieve()
 			.bodyToFlux(MarketCodeResponse.class)
+			.collectList()
+			.block();
+	}
+
+	public List<TickerResponse> getTickers(List<String> markets) {
+		String marketParam = String.join(",", markets);
+		return webClient.get()
+			.uri(uriBuilder -> uriBuilder
+				.path("/v1/ticker")
+				.queryParam("markets", marketParam)
+				.build())
+			.retrieve()
+			.bodyToFlux(TickerResponse.class)
 			.collectList()
 			.block();
 	}
--- src/main/java/com/tickit/batch/adapter/upbit/dto/TickerResponse.java
+++ src/main/java/com/tickit/batch/adapter/upbit/dto/TickerResponse.java
@@ -1,4 +1,32 @@
 package com.tickit.batch.adapter.upbit.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TickerResponse {
+
+	@JsonProperty("market")
+	private String market;
+
+	@JsonProperty("trade_price")
+	private Double tradePrice;
+
+	@JsonProperty("signed_change_price")
+	private Double signedChangePrice;
+
+	@JsonProperty("signed_change_rate")
+	private Double signedChangeRate;
+
+	@JsonProperty("acc_trade_volume")
+	private Double accTradeVolume;
+
+	@JsonProperty("timestamp")
+	private Long timestamp;
 }
