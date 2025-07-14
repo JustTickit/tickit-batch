f33ebd98ff5ddae670bdcbcb188ecc8bcfddfea9
희은 <datakubit2@gmail.com>
2025-07-15 02:56:50 +0900

    refactor: TickerWriter에 중복 데이터 필터링 로직 구현

--- src/main/java/com/tickit/batch/repository/TickerRepository.java
+++ src/main/java/com/tickit/batch/repository/TickerRepository.java
@@ -1,10 +1,17 @@
 package com.tickit.batch.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.tickit.batch.domain.Ticker;
 
 @Repository
 public interface TickerRepository extends JpaRepository<Ticker, Long> {
+
+	boolean existsByMarketAndTimestamp(String market, LocalDateTime timestamp);
 }
