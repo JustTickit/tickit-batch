33485ba31c58dc200b00cf8ae1c02800aa5f129e
희은 <datakubit2@gmail.com>
2025-07-23 04:42:11 +0900

    feat: 앱 시작 시, MarketCodeJob 실행 후 TickerJob이 실행되도록 구성

--- src/main/java/com/tickit/batch/TickitBatchApplication.java
+++ src/main/java/com/tickit/batch/TickitBatchApplication.java
@@ -1,15 +1,18 @@
 package com.tickit.batch;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableBatchProcessing
 @EnableScheduling
+@EnableRabbit
 public class TickitBatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TickitBatchApplication.class, args);
 	}
-
 }
--- src/main/java/com/tickit/batch/launch/BatchJobLauncher.java
+++ src/main/java/com/tickit/batch/launch/BatchJobLauncher.java
@@ -1,14 +1,42 @@
 package com.tickit.batch.launch;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
-@ComponentScan(basePackages = "com.tickit.batch")
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class BatchJobLauncher {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BatchJobLauncher.class, args);
+	private final JobLauncher jobLauncher;
+
+	@Qualifier("marketCodeJob") 
+	private final Job marketCodeJob;
+
+	@Bean
+	public CommandLineRunner runMarketCodeJob() {
+		return args -> {
+			try {
+				log.info("[Launcher] MarketCodeJob 실행 시작");
+
+				JobParameters params = new JobParametersBuilder()
+					.addLong("run.id", System.currentTimeMillis())
+					.toJobParameters();
+
+				jobLauncher.run(marketCodeJob, params);
+				
+				log.info("[Launcher] MarketCodeJob 실행 완료");
+			} catch (Exception e) {
+				log.error("[Launcher] MarketCodeJob 실행 실패: ", e);
+			}
+		};
 	}
 }
